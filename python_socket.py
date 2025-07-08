d75b04eee66c9511e485a960b98fcdaa82591fcc
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2025-07-08 10:17:48 -0500

    Update python_socket.py

--- python_socket.py
+++ python_socket.py
@@ -26,3 +26,18 @@ while True:
 
     # Close the client socket
     client_socket.close()
+
+
+
+
+#SOCKETS - Sockets can be used to connect two nodes together.  
+#import socket
+
+#HOST = '127.0.0.1'
+#PORT = 7777
+
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #af_inet is ipv4, sock stream is a port
+#s.connect((HOST,PORT))
+
+
+#nc -nvlp 7777
